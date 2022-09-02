06677c760b64e199eb437cf12b672eef5ace23cf
Vijay Kumar <sharmavijay86@gmail.com>
2022-09-03 00:35:29 +0530

    class

---
+++ terraform/lab-rgvnet/rgvnet.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "=3.0.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name     = "krlabdemo"
+    storage_account_name    = "krlabtfstate"
+    container_name          = "tfstate"
+    key                     = "lab.terraform.tfstate"
+  }
+}
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "krlabrg" {
+  name     = "krlab"
+  location = "eastus"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "labvnet" {
+  name                = "labvnet"
+  resource_group_name = azurerm_resource_group.krlabrg.name
+  location            = azurerm_resource_group.krlabrg.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Create a vm subnet 
+resource "azurerm_subnet" "vmsubnet" {
+  name                 = "vmsubnet"
+  resource_group_name  = azurerm_resource_group.krlabrg.name
+  virtual_network_name = azurerm_virtual_network.labvnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+# Create a dev subnet 
+resource "azurerm_subnet" "devsubnet" {
+  name                 = "devsubnet"
+  resource_group_name  = azurerm_resource_group.krlabrg.name
+  virtual_network_name = azurerm_virtual_network.labvnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+# Create a vm subnet 
+resource "azurerm_subnet" "prodsubnet" {
+  name                 = "prodsubnet"
+  resource_group_name  = azurerm_resource_group.krlabrg.name
+  virtual_network_name = azurerm_virtual_network.labvnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
