85030de197b197345e82242b78703d6cfb06b1c4
Vijay K <sharmavijay86@gmail.com>
2022-12-11 15:49:53 +0530

    Create github-repo.tf

---
+++ terraform/github-repo.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    github = {
+      source = "integrations/github"
+      version = "5.12.0"
+    }
+  }
+  backend "http" {}
+}
+
+provider "github" {
+  # Configuration options
+}
+
+resource "github_repository" "example" {
+  name        = "example"
+  description = "My awesome codebase"
+  visibility  = "public"
+  auto_init   = true
+}
