6d6a8e65eb433bbe4bf19aa4208f64fa7ed3841e
Vijay K <sharmavijay86@gmail.com>
2023-10-22 15:52:25 +0530

    Update main.tf

--- terraform/rgvnetvm/main.tf
+++ terraform/rgvnetvm/main.tf
@@ -17,10 +17,10 @@ terraform {
 provider "azurerm" {
   use_oidc = true
   features {
-│     resource_group {
-│       prevent_deletion_if_contains_resources = false
-│     }
-│   }
+     resource_group {
+       prevent_deletion_if_contains_resources = false
+     }
+   }
 }
 
 variable "VMCOUNT" {
